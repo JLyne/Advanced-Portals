1da580e1aae96b864da75c029e7f385b1b9ccb34
James Lyne <jim+github@not-null.co.uk>
2025-03-02 18:09:50 +0000

    Remove unused code

--- src/main/java/com/sekwah/advancedportals/bukkit/AdvancedPortalsPlugin.java
+++ src/main/java/com/sekwah/advancedportals/bukkit/AdvancedPortalsPlugin.java
@@ -14,7 +14,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -113,8 +112,6 @@ public class AdvancedPortalsPlugin extends JavaPlugin {
         // Enables very basic bungee support if not setup right
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         if(registerProxyChannels || this.checkIfBungee()) {
-            this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new BungeeListener(this));
-
             this.getServer().getMessenger().registerOutgoingPluginChannel(this, BungeeMessages.CHANNEL_NAME);
             this.getServer().getMessenger().registerIncomingPluginChannel(this, BungeeMessages.CHANNEL_NAME, new PluginMessageReceiver(this));
         }
